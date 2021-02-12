f23a242def73a7da3182f6b4585f29493b9b7011
Yash Singh <yash.singh5445@gmail.com>
2021-02-12 17:27:32 +0530

    added nodejs

---
+++ server/src/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
+
+const middlewares = require('./middlewares');
+
+const app = express();
+
+app.use(morgan('common'));
+app.use(helmet());
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+  })
+);
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'hello nodejs',
+  });
+});
+
+app.use(middlewares.notFound);
+app.use(middlewares.errorHandler);
+
+const port = process.env.PORT || 1337;
+app.listen(port, () => {
+  console.log(`App running on port = ${port}`);
+});
